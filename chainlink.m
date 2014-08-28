0453d66d5cc547dfc4361184cc0758654737f71f
Sidharth Iyer <246964@gmail.com>
2014-08-28 07:09:40 +0530

    Vectorized GA, workarounds for MATLAB's column-major matrix issues

--- chainlink.m
+++ chainlink.m
@@ -1,29 +1,90 @@
-function [spread, H_xi]  = chainlink(N)
-% CHAINLINK calculates separation and overlap factor for given node configuration N in Euclidean space
-% Input: N[3*m] = [Cx1 Cy1 Cz1 ... Cxm Cym Czm]
+%% CHAINLINK
+% Calculates the volume and separations for given node configuration.
 
-% TODO: Investigate/implement z_ij from Page 14 of Uhler_sphere_packing.pdf
+%% Signature
+function volume = chainlink(N, R)
+%%
+% Objective: Maximize volume of polyhedron defined by
+% a set of coordinates _N_(_r_, :), subject to edge coverage constraints.
+%%
+% Input: Vectorized array of individuals _N_(_INDIVS_, 3 * _NUM_),
+% such that _N_(_r_, :) = [ _Cx1_ _Cy1_ _Cz1_ ... _Cx<NUM>_ _Cy<NUM>_ _Cz<NUM>_ ],
+% vector of base coverage radii of nodes _R_
 
-    global M R;
+    %% Preparing the output vector for vectorized input
+    INDIVS = size(N, 1);        % Number of incoming individuals
+    NUM = size(N, 2) / 3;       % Number of nodes
+    volume = zeros(INDIVS, 1);  % Column vector for vectorized scores
 
-    % m = numel(N) / 3; % Number of nodes
-    sep = zeros(1, nchoosek(M, 2)); % Number of lattice edges
-    xi = zeros(1, nchoosek(M, 2)); % Number of overlaps
-    idx = 1; % Index of lattice interactions
+    %% Iterating over each individual in the vectorized input
+    for i = 1 : INDIVS
+        inferior = false;   % Flag for current individual's status
 
-    for i = 0:M-2
-        for j = i+1:M-1
-            % Calculate and append distance between centres of nodes i and j
-            sep(idx) = norm(N(i*3+(1:3)) - N(j*3+(1:3)));
-            % Calculate and append overlap between nodes i and j
-            xi(idx) = R(i+1) + R(j+1) - sep(idx);
-            idx = idx + 1;
+        %%
+        % Reformatting each individual into a convenient 2D matrix
+        % Reshape _N_(_m_, :) as _N2_(_NUM_, 3),
+        % such that row vector _N2_(_r_, :) = [ _Cx_ _Cy_ _Cz_ ]
+
+        % Workaround for MATLAB's column-major matrix policy
+        N2 = reshape(N(i,:), 3, NUM)';
+
+        %% Calculating the volume of the point cloud polyhedron
+        % Use Delaunay triangulation to create a tetrahedral mesh,
+        % and find the facets and volume of the convex hull over it:
+
+        % TODO: Investigate concave polyhedron volume calculation algorithms
+        DT = delaunayTriangulation(N2);
+        [facets, volume(i)] = convexHull(DT);
+
+        %% Constraint checking
+        % Ensure that there are no gaps in coverage on any edge of any facet.
+        % Edge coverage is considered to be complete if the coverage ranges of
+        % both nodes at the involved adjacent vertices overlap or at least meet
+        % at some point on their common edge.
+
+        % Enumerate and iterate over each adjacent vertex pair of each facet:
+        numFacets = size(facets, 1);
+        numVerts = size(facets, 2);
+
+        for f = 1 : numFacets
+            for v1 = 1 : numVerts
+                v2 = rem(v1, numVerts) + 1;     % Round robin pair traversal
+                p1 = facets(f,v1);
+                p2 = facets(f,v2);
+
+                %%
+                % Calculate the separation between two adjacent vertices
+                % and the total coverage along their common edge:
+                edge = norm(N2(p1,:) - N2(p2,:));
+                edgeCover = R(p1) + R(p2);
+
+                %% Defining the penalty for edge coverage gap
+                % _volume_(_i_) = _volume_(_i_) + _edge_ - _edgeCover_ is insufficient
+                % because as the volume increases cubically,
+                % it easily offsets the linear increase in penalty.
+                % Hence, reset the score to zero as soon as the first edge gap is found,
+                % and escape from the inferior individual's loop.
+                if edgeCover < edge
+                    % TODO: Alternatives to zero-tolerance scheme for gaps
+                    volume(i) = 0;
+                    %%
+                    % Escape inferior individual's loop _(1/3)_
+                    inferior = true;
+                    break
+                end
+            end
+            %%
+            % Escape inferior individual's loop _(2/3)_
+            if inferior == true
+                break
+            end
+        end
+        %%
+        % Escape inferior individual's loop _(3/3)_
+        if inferior == true
+            continue
         end
     end
 
-    % Return negative of mean separation
-    spread = -mean(sep);
-    % Return 2-norm of overlaps
-    H_xi = norm(xi);
-
+%% Return the score vector of the input individuals
 end
--- stretch_chainlink.m
+++ stretch_chainlink.m
@@ -1,37 +1,63 @@
-function x = stretch_chainlink()
-% STRETCH_CHAINLINK uses multi-objective GA to optimize CHAINLINK objectives
+%% STRETCH_CHAINLINK
+% Uses a vectorized genetic algorithm to optimize _CHAINLINK_.
 
-% OBSOLETE: Input: N[4][m] such that column subvector N(1:4,col) = [Cx Cy Cz R]
+%% Signature
+function N = stretch_chainlink()
+% TODO: Implement anisotropic attenuation
+% TODO: Input: Vector of base coverage radii of nodes R
 
-    N = [49 30 -47 94; 12 28 -34 62; 29 4 -20 93; 38 84 -30 16];
+    %% Initializing global variables
+    % Hardcoded values for testing and debugging:
 
-    global M R;
-    M = size(N, 1);
-    R = N(4, 1:M)
+    % R = [94 62 93 16]             % Random radii for 4 nodes
+    R = [13 25 18 42 25 32 24 24]   % Random radii for 8 nodes
 
-    % FIXME: Set options properly
-    options = gaoptimset('PopulationSize', 100); % , ...
-              % 'ParetoFraction', 0.7, 'PlotFcns', @gaplotpareto);
+    NUM = size(R, 2)                % Number of nodes
 
-    % FIXME: Set constraints, ranges, initial population, etc.
-    [x, fval, flag, output, population] = gamultiobj(@chainlink, 3*M, ...
-                                          [], [], [], [], [-5,-5], [5,5], options);
+    %% Genetic algorithm options
+    % _TolFun_ sets stopping criterion based on average change of
+    % fitness function value over _StallGen_ generations to the specified value.
+    %%
+    % _Vectorized_ specifies if the the GA is to be run with
+    % multiple individuals passed to it for evaluation at once or not.
 
-    fval
-    output
+    % TODO: Set options optimally
+    oldopts = gaoptimset(@ga);      % Load default options explicitly
+    newopts = ...
+        struct( ...
+            'TolFun',       1e-6, ...               % Default: 1e-6
+            'PlotFcns',     { @gaplotbestf }, ...   % Default: []
+            'Display',      'iter', ...             % 'iter', default: 'final'
+            'Vectorized',   'on' ...                % Default: 'off'
+              );
+    options = gaoptimset(oldopts, newopts);     % Overwrite selected parameters
 
-    % Present as x[3][m] such that column subvector x(1:3,col) = [Cx Cy Cz]
-	x = reshape(x, 3, 4);
+    %% Invoking the genetic algorithm
+    % Maximize _CHAINLINK_ by minimizing its negative score:
+    tic    % Start timer
+    objFunc = @(N) -chainlink(N, R);    % Create function handle for GA
+    %%
+    % Dump GA output: [ _x_, _fval_, _exitFlag_, _output_, _population_, _scores_ ]
+    [N, volume, ~, output, ~, ~] = ga(objFunc, 3 * NUM, options)
+    toc    % Poll timer
 
-    dist = zeros(M,M);
+    %%
+    % Calculate map separation between each pair of nodes.
+    % Reshape _N_(1, 3 * _NUM_) as _N_(_NUM_, 3),
+    % such that row vector _N_(_r_, :) = [ _Cx_ _Cy_ _Cz_ ]
 
-    for i = 1:M-1
-        for j = i:M
-            dist(i,j) = norm(x(1:3,i) - x(1:3,j));
+    % Workaround for MATLAB's column-major matrix policy
+	N = reshape(N, 3, NUM)';
+
+    sep = zeros(NUM, NUM);
+
+    for i = 1 : NUM - 1
+        for j = i + 1 : NUM
+            sep(j,i) = norm(N(i,:) - N(j,:));
         end
     end
 
-    % Map distances between nodes i and j
-    dist
+    sep
 
+%% Return volume optimized configuration for the given number of nodes
 end
