daa04c8629dce2b6482c1b57e876d93127d62612
Sidharth Iyer <246964@gmail.com>
2014-09-09 01:29:48 +0530

    More organized code flow

--- francois_garrison.m
+++ francois_garrison.m
@@ -1,9 +1,10 @@
 function range = francois_garrison(T, S, z, pH, f)
+% Calculates anisotropically attenuated node coverage ranges
 % Based on Lurton Sec. 2.3
 %
 % Examples:
 %   FRANCOIS_GARRISON
-%     Default parameters  
+%     Default parameters
 %   FRANCOIS_GARRISON(14, 38.5, 0, 7, 100)
 %     Mediterranean approximation at surface from Lurton
 %
@@ -15,51 +16,51 @@ function range = francois_garrison(T, S, z, pH, f)
   if nargin == 0
     T = 25;   % degrees Celsius
     S = 35;   % practical salinity units
-    z = 0;    % metres
+    z = 10;   % metres
     pH = 7;
-    f = 100;  % kHz
+    f = 10;  % kHz
   end
-  
+
   tic
 
+  % Speed of sound:
   c = calc_c(T, S, z);
 
   % Boric acid:
-  A1 = calc_A1(c, pH);
+  A1 = calc_A1(pH, c);
   P1 = 1;
-  f1 = calc_f1(S, T);
+  f1 = calc_f1(T, S);
 
   % Magnesium sulphate:
-  A2 = calc_A2(S, c, T);
+  A2 = calc_A2(T, S, c);
   P2 = calc_P2(z);
   f2 = calc_f2(T, S);
 
   % Pure water:
-  P3 = calc_P3(z);
   A3 = calc_A3(T);
+  P3 = calc_P3(z);
 
   % Attenuation in dB/km:
-  alpha = calc_zCoeff(z) * calc_alpha(A1, P1, f1, f, A2, P2, f2, A3, P3);
+  alpha = calc_zCoeff(z) * calc_alpha(A1, A2, A3, P1, P2, P3, f, f1, f2);
   display(alpha);
 
   % Define acceptable loss in intensity in dB:
-  SL = 145;       % Source level
+  SL = 150;       % Source level: [143,153]
   DT = 0;         % Detection threshold
   TL = SL - DT;   % Maximum acceptable trasmission loss
 
   syms R positive;
 
-  % Two-way transmission loss:
-  range = eval(solve(TL == 2 * (20 * log10(R) + alpha * 10 ^ -3 * R), ...
-                     R, 'Real', true, 'PrincipalValue', true));
+  % 1. a) Two-way transmission loss:
+  f = symfun(TL == 2 * (20 * log10(R) + alpha * 1e-3 * R), R);
 
-  % Two-way transmission loss (more intuitive result?):
-  % range = eval(solve(TL == 20 * log10(2 * R) + alpha * 0.001 * 2 * R, ...
-  %                    R, 'Real', true, 'PrincipalValue', true));
+  % 1. b) Two-way transmission loss (more intuitive result?):
+  % f = symfun(TL == 20 * log10(2 * R) + alpha * 1e-3 * 2 * R, R);
 
-	% One-way transmission loss:
-  % range = eval(solve(TL == 20 * log10(R) + alpha * 0.001 * R, ...
-  %                    R, 'Real', true, 'PrincipalValue', true));
+  % 2. One-way transmission loss:
+  % f = symfun(TL == 20 * log10(R) + alpha * 1e-3 * R, R);
+
+  range = eval(solve(f, 'Real', true, 'PrincipalValue', true));
 
   toc
 
@@ -69,32 +70,16 @@ function c = calc_c(T, S, z)
   c = 1412 + 3.21 * T + 1.19 * S + 0.0167 * z;
 end
 
-function A1 = calc_A1(c, pH)
+function A1 = calc_A1(pH, c)
   A1 = (8.86 / c) * 10 ^ (0.78 * pH - 5);
 end
 
-function f1 = calc_f1(S, T)
-  f1 = 2.8 * sqrt(S / 35) * 10 ^ (4 - 1245 / (T + 273));
-end
-
-function A2 = calc_A2(S, c, T)
+function A2 = calc_A2(T, S, c)
   A2 = 21.44 * (S / c) * (1 + 0.025 * T);
 end
 
-function P2 = calc_P2(z)
-  P2 = 1 - 1.37e-4 * z + 6.2e-9 * z ^ 2;
-end
-
-function f2 = calc_f2(T, S)
-  f2 = (8.17 * 10 ^ (8 - 1990 / (T + 273))) / (1 + 0.0018 * (S - 35));
-end
-
-function P3 = calc_P3(z)
-  P3 = 1 - 3.83e-5 * z + 4.9e-10 * z ^ 2;
-end
-
 function A3 = calc_A3(T)
-  % Taking Lurton's T < 20 to mean T <= 20:
+  % Taking Lurton's T < 20 as T <= 20:
   if T <= 20
     A3 = 4.937e-4 - 2.59e-5 * T + 9.11e-7 * T ^ 2 - 1.5e-8 * T ^ 3;
   else
@@ -102,7 +87,23 @@ function A3 = calc_A3(T)
   end
 end
 
-function alpha = calc_alpha(A1, P1, f1, f, A2, P2, f2, A3, P3)
+function f1 = calc_f1(T, S)
+  f1 = 2.8 * sqrt(S / 35) * 10 ^ (4 - 1245 / (T + 273));
+end
+
+function f2 = calc_f2(T, S)
+  f2 = (8.17 * 10 ^ (8 - 1990 / (T + 273))) / (1 + 0.0018 * (S - 35));
+end
+
+function P2 = calc_P2(z)
+  P2 = 1 - 1.37e-4 * z + 6.2e-9 * z ^ 2;
+end
+
+function P3 = calc_P3(z)
+  P3 = 1 - 3.83e-5 * z + 4.9e-10 * z ^ 2;
+end
+
+function alpha = calc_alpha(A1, A2, A3, P1, P2, P3, f, f1, f2)
   alpha = A1 * P1 * f1 * f ^ 2 / (f1 ^ 2 + f ^ 2) ...
         + A2 * P2 * f2 * f ^ 2 / (f2 ^ 2 + f ^ 2) ...
         + A3 * P3 * f ^ 2;
